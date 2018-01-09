97c4de7905a851df8d28be967ca7c2f1c34cbeba
babyhana <babyhana@babyhana-no-MacBook-Pro.local>
2018-01-09 14:41:33 +0900

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,18 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
+  def about_us
+  end
+  
+  def menu
+  end
+  
+  def access
+  end
+  
+  def news
+  end
+  
+  def company
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+ get 'static_pages/home'
+ get 'static_pages/about_us'
+ get 'static_pages/menu'
+ get 'static_pages/access'
+ get 'static_pages/news'
+ get 'static_pages/company'
+ 
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
